95c1c520904b1aa4b9aabb28765368a9e421bb4c
hejun <hejun@Ctrip.com>
2015-01-14 20:23:40 +0800

    项目修改：提取文档目录中的指定文件类型的文件名及版本信息以及引用路径

--- GetDLLVersion/GetDLLVersion/Program.cs
+++ GetDLLVersion/GetDLLVersion/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace GetDLLVersion
 {
@@ -11,13 +12,16 @@ namespace GetDLLVersion
     {
         static void Main(string[] args)
         {
-            string file = @"D:\Ground\Activity\Vendor\VendorService\Activity.Vendor.VendorService.Service\bin";
-            string shortfile = "VendorService_Activity.Vendor.VendorService.Service";
-            StreamWriter sw = new StreamWriter(@"d:\Users\hejun\Desktop\" + shortfile + ".js", false, System.Text.UTF8Encoding.UTF8);
+            string file = @"D:\Ground\Activity\Booking\BookingJobWS\Activity.Booking.BookingJobWS\bin\Debug";
+            string shortfile = "BookingJobWS_Activity.Booking.BookingJobWS";
+
+            string pathfile = @"D:\Ground\Activity\Booking\BookingJobWS\Activity.Booking.BookingJobWS";
+
+            StreamWriter sw = new StreamWriter(@"d:\Users\hejun\Desktop\" + shortfile + ".csv", false, System.Text.UTF8Encoding.UTF8);
             Console.SetOut(sw);
             
-            // 获取目录内的文件名及版本信息
-            GetFileCount(file, ".dll");
+            // 获取目录内的文件名及版本信息以及引用文件路径
+            GetFile(file, ".dll", pathfile, ".csproj");
 
             sw.Close();
         }
@@ -25,17 +29,58 @@ namespace GetDLLVersion
         // 参数：
         // string dir 指定的文件夹
         // string ext 文件类型的扩展名，如".dll", ".txt" , “.exe"
-        static void GetFileCount(string dir, string ext)
+        static void GetFile(string dir, string ext, string pathdir, string pathext)
         {
-            DirectoryInfo dirinfo = new DirectoryInfo(dir);
+            DirectoryInfo dirinfo = new DirectoryInfo(dir);             // bin目录信息
+            DirectoryInfo pathdirinfo = new DirectoryInfo(pathdir);     // 查询.csproj文件
+
+            List<string> dllpath = new List<string>();                  // 存储dll文件的路径
+
+            foreach (FileInfo pathfi in pathdirinfo.GetFiles())
+            {
+                if (pathfi.Extension.ToUpper() == pathext.ToUpper())    // 查询.csproj文件
+                {
+                    FileStream fs = new FileStream(pathfi.FullName, FileMode.OpenOrCreate);
+                    StreamReader sr = new StreamReader(fs, Encoding.Default);
+ 
+                    while (!sr.EndOfStream)                             // 读到结尾退出
+                    {
+                        string temp = sr.ReadLine();
+                        if (temp.Contains(".dll"))
+                        {
+                            string regexStr = @"\.\.\\\.\.\\.*?.dll";
+                            Regex reg = new Regex(regexStr);
+                            Match mc = reg.Match(temp);
+
+                            dllpath.Add(mc.Value);
+                        }
+                    }
+                    fs.Close();
+                    sr.Close();
+                }
+            }
+
             foreach (FileInfo fi in dirinfo.GetFiles())
             {
                 if (fi.Extension.ToUpper() == ext.ToUpper())
                 {
                     FileVersionInfo myFileVersion = FileVersionInfo.GetVersionInfo(fi.FullName);
-                    string Text = myFileVersion.FileVersion; 
+                    string dllName = fi.Name;
+                    string Text = myFileVersion.FileVersion;
+                    string tmp = "";
+                    for (int i = 0; i < dllpath.Count; i++)
+                    {
+                        if (dllpath[i].Contains(dllName))
+                        {
+                            tmp = dllpath[i];
+                            dllpath[i] = "null";
+                            break;
+                        }
+                        else
+                            tmp = "NULL";
+                    }
 
-                    Console.WriteLine(fi.Name + "   " + Text);
+                    Console.WriteLine(dllName + "," + Text + "," + tmp);
                 }
             }
         }
